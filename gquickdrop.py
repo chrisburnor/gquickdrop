84c0b30798e0bb4f4b90a3ec2cc552f5569cdaad
Chris Burnor <christopher.burnor@gmail.com>
2010-10-31 22:42:33 -0700

    Command Line interface to authorize and determine if file determined by arguments is already there

--- gquickdrop.py
+++ gquickdrop.py
@@ -1,15 +1,23 @@
 from dropbox.auth import Authenticator
 from dropbox.client import DropboxClient
 
-import webbrowser
+import webbrowser, sys
+
+class FileAlreadyPresentError(Exception):
+    def __init__(self, fileName):
+        self.fileName = fileName
+        
+    def __str__(self):
+        return repr(self.fileName)
 
 def openClient():
     config = Authenticator.load_config("config.ini")
     auth = Authenticator(config)
     reqToken = auth.obtain_request_token()
     print "Getting Authorization Token"
+    # Need to replace this with gtkmozembed from pygtk.com
     webbrowser.open(auth.build_authorize_url(reqToken))
-    raw_input("\nPress Enter when you have authorized...\n")
+    raw_input("\nPress Enter when you have authorized   \n")
     accToken = auth.obtain_access_token(reqToken, "")
     print "Token: %s" % str(accToken)
     client = DropboxClient("api.dropbox.com", "api-content.dropbox.com", 80, auth, accToken)
@@ -17,11 +25,26 @@ def openClient():
     return client
 
 if __name__ == "__main__":
-    client = openClient()
-    publicMetadata = client.metadata("dropbox", "/Public")
-    if publicMetadata != 404:
-        print "Loaded Public directory..."
+    try:
+        myFile = sys.argv[1]
+    except IndexError:
+        print "Error, you must specify a file to upload"
     else:
-        print "Public directory not found..."
+        client = openClient()
+        
+        public = client.metadata("dropbox", "/Public")
+        if public.status != 404:
+            print "Loaded Public directory   "
+            try:
+                for pubFile in public.data['contents']:
+                    if pubFile['path'].split("/")[-1] == myFile:
+                        raise FileAlreadyPresentError(myFile)
+            except FileAlreadyPresentError as e:
+                print "File %s already present" % e.fileName
+            else:
+                print "File Not Present"
+                        
+        else:
+            print "Public directory not found"
     
 

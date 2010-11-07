1b2bcd503c55941757cc14dfeb934c4d4302f1ee
Chris Burnor <christopher.burnor@gmail.com>
2010-11-07 09:56:15 -0800

    Added uploadFile, getFileURL methods. These are mostly template methods. Haven't tested them and I'm sure they'll need re-writing

--- gquickdrop.py
+++ gquickdrop.py
@@ -9,20 +9,38 @@ class FileAlreadyPresentError(Exception):
         
     def __str__(self):
         return repr(self.fileName)
+    
+def getAccToken(reqToken):
+    print "Getting Authorization Token"
+    ######################################################
+    # Need to replace this with gtkmozembed from pygtk.com
+    webbrowser.open(auth.build_authorize_url(reqToken))
+    raw_input("\nPress Enter when you have authorized   \n")
+    ######################################################
+    accToken = auth.obtain_access_token(reqToken, "")
+    return accToken
 
 def openClient():
     config = Authenticator.load_config("config.ini")
     auth = Authenticator(config)
     reqToken = auth.obtain_request_token()
-    print "Getting Authorization Token"
-    # Need to replace this with gtkmozembed from pygtk.com
-    webbrowser.open(auth.build_authorize_url(reqToken))
-    raw_input("\nPress Enter when you have authorized   \n")
-    accToken = auth.obtain_access_token(reqToken, "")
+    accToken = getAccToken(reqToken)
     print "Token: %s" % str(accToken)
     client = DropboxClient("api.dropbox.com", "api-content.dropbox.com", 80, auth, accToken)
     
     return client
+    
+def uploadFile(fileName, client):
+    print "Uploading %s" % fileName
+    fp = open(fileName, "r")
+    resp = client.put_file("dropbox", "/Public", fp);
+    fp.close()
+    
+def getPublicUrl(fileName, client):
+    fileMetaData = client.metadata("dropbox", "/Public/%s" % fileName)
+    publicUrl = fileMetaData['path']
+    return publicUrl
+    
 
 if __name__ == "__main__":
     try:
@@ -43,6 +61,9 @@ if __name__ == "__main__":
                 print "File %s already present" % e.fileName
             else:
                 print "File Not Present"
+                uploadFile(myFile, client)
+            print "File URL: %s" % getPublicUrl(e.fileName, client)
+        
                         
         else:
             print "Public directory not found"
